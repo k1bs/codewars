5270ec760e7fb6916562a421d3990e91d80f7780
Alex Kibler <alexkibler@me.com>
2018-06-25 07:44:23 -0600

    working on kata

--- scratch.rb
+++ scratch.rb
@@ -9,7 +9,7 @@ def increment_string(input)
   letters + num.to_s.rjust(length, '0')
 end
 
-puts increment_string('f00bar01')
+# puts increment_string('f00bar01')
 
 def race(vel1, vel2, lead)
   return [0, 0, 0] if lead.zero?
@@ -22,4 +22,17 @@ def race(vel1, vel2, lead)
   [hours.floor, minutes.floor, seconds.floor]
 end
 
-puts race(81, 100, 40)
+# puts race(81, 100, 40)
+
+def sum_pairs(ints, s)
+  ints.length.times do |iter|
+    ints.map.with_index do |item, index|
+      unless ints[index + iter + 1].nil?
+        return [item, ints[index + iter + 1]] if (item + ints[index + iter + 1]) == s
+      end
+    end
+  end
+  nil
+end
+
+puts sum_pairs([1, 2, 3, 4, 5], 5)
