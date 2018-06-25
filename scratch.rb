91cb774e6a32bf2f46f51989f031115e8e55c9dc
Alex Kibler <alexkibler@me.com>
2018-06-25 07:11:49 -0600

    complete tortoise racing kata

--- scratch.rb
+++ scratch.rb
@@ -10,3 +10,16 @@ def increment_string(input)
 end
 
 puts increment_string('f00bar01')
+
+def race(vel1, vel2, lead)
+  return [0, 0, 0] if lead.zero?
+  return nil if vel1 >= vel2
+  gap = vel2.to_f - vel1
+  ratio = gap / lead.to_f
+  hours = 1 / ratio
+  minutes = hours.modulo(1) * 60
+  seconds = minutes.modulo(1) * 60
+  [hours.floor, minutes.floor, seconds.floor]
+end
+
+puts race(81, 100, 40)
